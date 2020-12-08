be661810d9911bfd2154937b5d255b7cd414a355
thomaslnx <marcos.msilva10@gmail.com>
2020-12-08 15:42:43 -0300

    Bossabox's backend challenge

---
+++ app/Controllers/Http/SwaggerTestController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class SwaggerTestController {
+  /**
+  * @swagger
+  * /api/hello:
+  *   get:
+  *     tags:
+  *       - Test
+  *     summary: Sample API
+  *     parameters:
+  *       - name: name
+  *         description: Name of the user
+  *         in: query
+  *         required: false
+  *         type: string
+  *     responses:
+  *       200:
+  *         description: Send hello message
+  *         example:
+  *           message: Hello Guess
+  */
+ async hello({ request, response }) {
+  const name = request.input('name', 'Guess')
+  response.send({ message: 'Hello ' + name })
+}
+}
+
+module.exports = SwaggerTestController
